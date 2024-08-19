6a84e80b791e16b33432bec707dbb19391ac0cec
neok <xpor2002@gmail.com>
2024-08-20 02:24:22 +0300

    Prepared for gif loading, but still need gif loading engine from external resource and specify, what is this, gif or jpeg

--- src/main/java/com/laytin/exlntab/render/PhotoRender.java
+++ src/main/java/com/laytin/exlntab/render/PhotoRender.java
@@ -1,13 +1,16 @@
 package com.laytin.exlntab.render;
 
+import com.laytin.exlntab.render.elem.GifAnim;
 import com.laytin.exlntab.utils.PhotoLoader;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.texture.DynamicTexture;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.StringUtils;
 import org.lwjgl.opengl.GL11;
 
 import java.awt.image.BufferedImage;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -15,36 +18,19 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class PhotoRender {
     protected final Minecraft mc = Minecraft.getMinecraft();
-    private ConcurrentHashMap<String, BufferedImage> cachedImages = new ConcurrentHashMap();
-    private ConcurrentHashMap<String, DynamicTexture> cachedTextures = new ConcurrentHashMap();
-    private List<String> fetchingSkins = Collections.synchronizedList(new ArrayList());
+    private ConcurrentHashMap<String, BufferedImage> skinImages = new ConcurrentHashMap();
+    private ConcurrentHashMap<String, ResourceLocation> skinImagesStream = new ConcurrentHashMap();
+    private ConcurrentHashMap<String, DynamicTexture> finalImages = new ConcurrentHashMap();
+    private ConcurrentHashMap<String, GifAnim> finalImagesGif = new ConcurrentHashMap();
+    private List<String> startUpload = Collections.synchronizedList(new ArrayList());
+    private List<String> endUpload = Collections.synchronizedList(new ArrayList());
     private static PhotoRender instance;
     public static PhotoRender getInstance() {
         if(instance==null)
             instance=new PhotoRender();
         return instance;
     }
-
-    private boolean bindFace(String username) {
-        if (this.fetchingSkins.contains(username)) {
-            if (this.cachedImages.containsKey(username)) {
-                DynamicTexture faceTex = new DynamicTexture((BufferedImage)this.cachedImages.get(username));
-                this.cachedTextures.put(username, faceTex);
-                this.cachedImages.remove(username);
-            }
-
-            return false;
-        } else if (!this.cachedTextures.containsKey(username)) {
-            this.fetchingSkins.add(username);
-            (new Thread(new PhotoLoader(username, this.cachedImages, this.fetchingSkins))).start();
-            return false;
-        } else {
-            GL11.glBindTexture(3553, ((DynamicTexture)this.cachedTextures.get(username)).getGlTextureId());
-            GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-            return true;
-        }
-    }
-    private void drawImage(int xPos, int yPos, int width, int height) {
+    private void drawImage(float xPos, float yPos, float width, float height) {
         Tessellator tessellator = Tessellator.instance;
         tessellator.startDrawingQuads();
         tessellator.addVertexWithUV((double)xPos, (double)(yPos + height), 0.0D, 0.0D, 1.0D);
@@ -53,13 +39,27 @@ public class PhotoRender {
         tessellator.addVertexWithUV((double)xPos, (double)yPos, 0.0D, 0.0D, 0.0D);
         tessellator.draw();
     }
-    public void drawAvatar(String username, int xPos, int yPos, int width, int height){
-        if (bindFace(StringUtils.stripControlCodes(username))) {
-/*            GL11.glPushMatrix();
-            GL11.glDisable(2929);*/
-            drawImage(xPos, yPos, 17, 17);
-/*            GL11.glPopMatrix();
-            GL11.glEnable(2929);*/
+    public void drawAvatar(String username, float xPos, float yPos, float width, float height,float alpha){
+        username = StringUtils.stripControlCodes(username);
+        if(finalImages.containsKey(username)) {
+            GL11.glPushMatrix();
+            GL11.glDisable(2929);
+            GL11.glBindTexture(3553, (this.finalImages.get(username)).getGlTextureId());
+            GL11.glColor4f(1.0F, 1.0F, 1.0F, alpha);
+            drawImage(xPos, yPos, width, height);
+            GL11.glPopMatrix();
+            GL11.glEnable(2929);
+        }else if (finalImagesGif.containsKey(username)){
+            finalImagesGif.get(username).gifPlay(xPos,yPos,width,height,1);
+            System.out.println("Try paint gif");
+        } else if(endUpload.contains(username)){
+            if(skinImages.containsKey(username))
+                finalImages.put(username,new DynamicTexture(skinImages.get(username)));
+            if(skinImagesStream.containsKey(username))
+                finalImagesGif.put(username,new GifAnim(skinImagesStream.get(username)));
+        } else if (!startUpload.contains(username)) {
+            startUpload.add(username);
+            (new Thread(new PhotoLoader(username, this.skinImages, this.endUpload))).start();
         }
     }
 }
