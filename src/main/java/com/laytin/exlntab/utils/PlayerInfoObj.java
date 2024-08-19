59c50280b5e7e358a4bafda99cb5b2023da57e3a
neok <xpor2002@gmail.com>
2024-08-20 02:22:30 +0300

    Player info obj, here we contain all data

---
+++ src/main/java/com/laytin/exlntab/utils/PlayerInfoObj.java
@@ -0,0 +1,50 @@
+package com.laytin.exlntab.utils;
+
+public class PlayerInfoObj {
+    private String role;
+    private String roleDisplayName;
+    private String username;
+    private int weight;
+
+    public PlayerInfoObj() {
+    }
+
+    public PlayerInfoObj(String role, String roleDisplayName, String username, int weight) {
+        this.role = role;
+        this.roleDisplayName = roleDisplayName;
+        this.username = username;
+        this.weight = weight;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getRoleDisplayName() {
+        return roleDisplayName;
+    }
+
+    public void setRoleDisplayName(String roleDisplayName) {
+        this.roleDisplayName = roleDisplayName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
