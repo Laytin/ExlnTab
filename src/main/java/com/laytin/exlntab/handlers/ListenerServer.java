f28462a69b5204833ac2651e8b7b0111a3eeff51
neok <xpor2002@gmail.com>
2024-08-29 02:59:59 +0300

    Migrated func from PluginAdapterUtil

--- src/main/java/com/laytin/exlntab/handlers/ListenerServer.java
+++ src/main/java/com/laytin/exlntab/handlers/ListenerServer.java
@@ -1,27 +1,76 @@
 package com.laytin.exlntab.handlers;
 
-import com.laytin.exlntab.bukkit.PluginAdapterUtil;
+import com.laytin.exlntab.bukkit.a.PluginAdapterLP;
+import com.laytin.exlntab.bukkit.a.PluginAdapterPEX;
+import com.laytin.exlntab.packets.AddPlayerListPacket;
+import com.laytin.exlntab.packets.FillPlayerListPacket;
+import com.laytin.exlntab.packets.RemovePlayerListPacket;
 import com.laytin.exlntab.utils.PlayerInfoObj;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.entity.player.EntityPlayerMP;
+import org.bukkit.Bukkit;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @SideOnly(Side.SERVER)
 public class ListenerServer {
     private Map<String, PlayerInfoObj> playerListTemp = new HashMap<>();
-    //private static PluginAdapterUtil injector = new PluginAdapterUtil();
+    private boolean inited = false;
+    private static int abc = 0;
+    private void init(){
+        if(Bukkit.getServer().getPluginManager().isPluginEnabled("PermissionEX")){
+            try {
+                PluginAdapterPEX.init();
+                System.out.println("[EXLNTab] Init PermissionEX plugin");
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            } catch (InstantiationException e) {
+                throw new RuntimeException(e);
+            }
+            abc = 1;
+        }else if(Bukkit.getServer().getPluginManager().isPluginEnabled("LuckPerms")){
+            try {
+                PluginAdapterLP.init();
+                System.out.println("[EXLNTab] Init LuckPerms plugin");
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            } catch (InstantiationException e) {
+                throw new RuntimeException(e);
+            }
+            abc = 2;
+        }else {
+            throw new RuntimeException("[EXLNTab] No permission plugin found. Just use PermissionEX or LuckPerms!");
+        }
+    }
 
     @SubscribeEvent
     public void onJoinEvent(PlayerEvent.PlayerLoggedInEvent event) {
-        //injector.getPlayerGroupWithWeight();
-        System.out.println("Logged in:"+event.player.getDisplayName());
+        if(!inited)
+            init();
+        PlayerInfoObj ib;
+        switch (abc) {
+            case 1:
+                ib = PluginAdapterPEX.getPlayerGroupWithWeight(event.player.getDisplayName());
+                break;
+            case 2:
+                ib = PluginAdapterLP.getPlayerGroupWithWeight(event.player.getDisplayName());
+                break;
+            default:
+                ib = new PlayerInfoObj("default", "Player", event.player.getDisplayName(), 0);
+                break;
+        }
+        playerListTemp.put(ib.getUsername(),ib);
+        //
+        new FillPlayerListPacket(new ArrayList<>(playerListTemp.values())).sendToPlayer((EntityPlayerMP) event.player);
+        //
+        new AddPlayerListPacket(ib).sendToClients();
     }
     @SubscribeEvent
     public void onLeftEvent(PlayerEvent.PlayerLoggedOutEvent event) {
-        System.out.println("Logged out:"+event.player.getDisplayName());
+        playerListTemp.remove(event.player.getDisplayName());
+        new RemovePlayerListPacket(event.player.getDisplayName()).sendToClients();
     }
 }
