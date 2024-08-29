aace88a7b7c78a0c90b220176fb1229184e12a7c
neok <xpor2002@gmail.com>
2024-08-29 03:46:02 +0300

    New tps packet

---
+++ src/main/java/com/laytin/exlntab/packets/TpsPacket.java
@@ -0,0 +1,23 @@
+package com.laytin.exlntab.packets;
+
+import com.laytin.exlntab.handlers.ListenerClient;
+import hohserg.elegant.networking.api.ElegantPacket;
+import hohserg.elegant.networking.api.ServerToClientPacket;
+import net.minecraft.client.Minecraft;
+
+import java.text.DecimalFormat;
+
+@ElegantPacket
+public class TpsPacket implements ServerToClientPacket {
+    final double tps;
+
+    public TpsPacket(double tps) {
+        this.tps = tps;
+    }
+
+    @Override
+    public void onReceive(Minecraft mc) {
+        DecimalFormat df = new DecimalFormat("##.00");
+        ListenerClient.tps = Double.parseDouble(df.format(tps));
+    }
+}
