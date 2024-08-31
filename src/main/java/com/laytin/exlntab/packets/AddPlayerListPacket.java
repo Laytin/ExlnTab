b97005ebd098d2a66d330544a615e73b1904f063
neok <xpor2002@gmail.com>
2024-08-31 16:02:25 +0300

    New logic

--- src/main/java/com/laytin/exlntab/packets/AddPlayerListPacket.java
+++ src/main/java/com/laytin/exlntab/packets/AddPlayerListPacket.java
@@ -5,8 +5,11 @@ import com.laytin.exlntab.utils.PlayerInfoObj;
 import hohserg.elegant.networking.api.ElegantPacket;
 import hohserg.elegant.networking.api.ServerToClientPacket;
 import net.minecraft.client.Minecraft;
+import ru.justagod.cutter.GradleSide;
+import ru.justagod.cutter.GradleSideOnly;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @ElegantPacket
 public class AddPlayerListPacket implements ServerToClientPacket {
@@ -15,32 +18,16 @@ public class AddPlayerListPacket implements ServerToClientPacket {
     public AddPlayerListPacket(PlayerInfoObj ib) {
         this.ib = ib;
     }
-
     @Override
     public void onReceive(Minecraft mc) {
         new Thread(()->{
             if (ib.getUsername().equals(mc.getSession().getUsername()))
                 return;
             Map<String, PlayerInfoObj> temp1 = new HashMap<>(ListenerClient.getInstance().playerList);
-            List<PlayerInfoObj> temp2 = new ArrayList<>(temp1.values());
-            boolean key = false;
-            for (int i = 0; i < temp2.size() + 1; i++) {
-                if (!key) {
-                    if (temp2.size() == i) //out of range
-                        temp1.put(ib.getUsername(), ib);
-
-                    if (temp2.get(i).getWeight() <= ib.getWeight()) {
-                        key = true;
-                        temp1.put(ib.getUsername(), ib);
-                    }else{
-                        PlayerInfoObj bb = temp2.get(i);
-                        temp1.put(bb.getUsername(), bb);
-                    }
-                } else {
-                    PlayerInfoObj bb = temp2.get(i - 1);
-                    temp1.put(bb.getUsername(), bb);
-                }
-            }
+            temp1.put(ib.getUsername(),ib);
+            List<PlayerInfoObj> temp2 = new ArrayList<>(temp1.values()).stream().sorted(Comparator.comparing(PlayerInfoObj::getWeight).reversed()).collect(Collectors.toList());
+            temp1.clear();
+            temp1 = temp2.stream().collect(Collectors.toMap(PlayerInfoObj::getUsername, playerInfoObj -> playerInfoObj));
             ListenerClient.getInstance().playerList = temp1;
         }).start();
     }
